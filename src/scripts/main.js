f4ad542526c1d5552a80afef945025d31e96daff
Oleh Lukash <lukasholeg02@gmail.com>
2024-12-14 12:45:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const employeeListItems = document.querySelectorAll('ul > li');
+
+const employees = Array.from(employeeListItems).map((item) => {
+  return {
+    name: item.textContent.trim(),
+    position: item.getAttribute('data-position'),
+    salary: Number(item.getAttribute('data-salary').replace(/[$,]/g, '')),
+    age: item.getAttribute('data-age'),
+  };
+});
+
+const sortList = function (list) {
+  list.sort((a, b) => b.salary - a.salary);
+};
+
+const getEmployees = function (list) {
+  return list;
+};
+
+sortList(employees);
+getEmployees(employees);
